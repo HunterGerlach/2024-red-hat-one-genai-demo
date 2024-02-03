401d43cb98c5f5f29098a0776dca941ee490e589
Hunter Gerlach <hunter@huntergerlach.com>
2024-02-02 22:30:59 -0500

    Expand config, redis support and add dev usage documentation

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # chat-doc-v1
+
+## Environment Setup
+
+### Python Streamlit Frontend Application
+
+python -m venv .venv-rh1
+
+source .venv-rh1/bin/activate
+
+pip install -r requirements.txt
+
+### Redis Vector Database Application
+
+- Enter the [redis-data](./redis-data/) directory: `cd redis-data`
+- Build the container: `podman build . -t redis-vector-db`
+- Run the container:
+
+```sh
+podman run --rm -d --name redis_vector_db -v $PWD/redis.conf:/usr/local/etc/redis/redis.conf -p 6379:6379 redis-vector-db:latest
+```
+
+- Run Redis Insights container to view the data in the db:
+
+```sh
+podman run -d --rm -v redisinsight:/db -p 8001:8001 redislabs/redisinsight:latest
+```
+
+## Application Setup
+
+Update config.yaml and, if necessary, replace the hashed password with your preferred password (use bcrypt to hash).
+
+## Run
+
+### Open browser window by default
+
+streamlit run app.py
+
+### Headless
+
+streamlit run app.py --server.headless true
+
+## Usage
+
+### Create a new chat
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 import streamlit as st
 from langchain.llms import HuggingFaceTextGenInference
+from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
 
 from gui.history import ChatHistory
@@ -9,26 +10,28 @@ from gui.layout import Layout
 from gui.sidebar import Sidebar, Utilities
 from snowflake import SnowflakeGenerator
 
-username = os.environ.get("REDIS_USERNAME", "default")
-password = os.environ.get("REDIS_PASSWORD", "default")
-host = os.environ.get("REDIS_HOST", "127.0.0.1")
-redis_url = f"redis://{username}:{password}@{host}:6379"
-
-inference_server_url=os.environ.get('INFERENCE_SERVER_URL',
-  'https://llama-2-7b-chat-hf-fine-tuned-predictor-rhone-chatbot-demo.apps.rosa-zpp6h.zjoc.p1.openshiftapps.com/')
-
 if __name__ == '__main__':
     st.set_page_config(layout="wide", page_icon="💬", page_title="ChatPDF")
     layout, sidebar, utils = Layout(), Sidebar(), Utilities()
 
-    layout.show_header()
-    sidebar.show_logo()
-    login_config = utils.load_login_details()
+    configs = utils.load_config_details()
+    
+    username = configs["redis"]["username"]
+    password = configs["redis"]["password"]
+    host = configs["redis"]["host"]
+    port = configs["redis"]["port"]
+    redis_url = f"redis://{username}:{password}@{host}:{port}"
 
-    if not login_config:
+    inference_server_url= configs["inference_server"]["url"]
+
+
+    layout.show_header()
+    if not configs:
         layout.show_loging_details_missing()
     else:
-        sidebar.show_login(login_config)
+        
+        sidebar.show_logo(configs)
+        sidebar.show_login(configs)
         pdf = utils.handle_upload()
 
         if pdf:
@@ -36,21 +39,24 @@ if __name__ == '__main__':
 
             try:
                 if 'chatbot' not in st.session_state:
-                    llm = HuggingFaceTextGenInference(
-                        inference_server_url=os.environ.get('INFERENCE_SERVER_URL'),
-                        max_new_tokens=int(os.environ.get('MAX_NEW_TOKENS', '512')),
-                        top_k=int(os.environ.get('TOP_K', '10')),
-                        top_p=float(os.environ.get('TOP_P', '0.95')),
-                        typical_p=float(os.environ.get('TYPICAL_P', '0.95')),
-                        temperature=float(os.environ.get('TEMPERATURE', '0.9')),
-                        repetition_penalty=float(os.environ.get('REPETITION_PENALTY', '1.01')),
-                        streaming=False,
-                        verbose=False
-                    )
+                    # llm = HuggingFaceTextGenInference(
+                    #     inference_server_url=os.environ.get('INFERENCE_SERVER_URL'),
+                    #     max_new_tokens=int(os.environ.get('MAX_NEW_TOKENS', '512')),
+                    #     top_k=int(os.environ.get('TOP_K', '10')),
+                    #     top_p=float(os.environ.get('TOP_P', '0.95')),
+                    #     typical_p=float(os.environ.get('TYPICAL_P', '0.95')),
+                    #     temperature=float(os.environ.get('TEMPERATURE', '0.9')),
+                    #     repetition_penalty=float(os.environ.get('REPETITION_PENALTY', '1.01')),
+                    #     streaming=False,
+                    #     verbose=False
+                    # )
+                    
+                    llm = Ollama(model="mistral")
 
                     indexGenerator = SnowflakeGenerator(42)
                     index_name = str(next(indexGenerator))
-
+                    print("Index Name: " + index_name)
+                    
                     chatbot = utils.setup_chatbot(pdf, llm, redis_url, index_name, "redis_schema.yaml")
                     st.session_state["chatbot"] = chatbot
 
